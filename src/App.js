de92f3a784cd385dce6749e572a7203954f1a6ea
rajdhage2104 <rajdhage2000@gmail.com>
2025-03-25 13:35:17 +0530

    new code added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegistrationForm from './registration';
+import SuccessPage from './SuccessPage';
+import UserList from './list';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={RegistrationForm}></Route>
+        <Route path='/success' Component={SuccessPage}></Route>
+        <Route path='/lists' Component={UserList}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
